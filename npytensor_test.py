0ff72cc06990fac0492daec233bf6e22987e784d
Alessandro Sciarrillo <alessandr.sciarrill2@studio.unibo.it>
2023-03-29 19:39:09 +0200

    directory refactor and clean

--- npytensor_test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-r = np.load('./heatmaps_npy/heatmaps_00000001_00000001.npy');
-
-print(r.ndim, r.shape, r.dtype, r.itemsize) # 3 (95, 511, 12) float32 4
-
-print(r[:,:,0])
-print(r[:,:,0].shape)
